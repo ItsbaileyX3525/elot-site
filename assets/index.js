3960b275f52c687ea8ba97363ebd7f5b35707c98
bailey <baileyleah542@gmail.com>
2025-03-11 02:44:18 +0000

    Add elot clicker

--- assets/index.js
+++ assets/index.js
@@ -2,12 +2,31 @@ console.log("Well well well, how did I know you were gonna see this, Dexter.");
 
 document.addEventListener("DOMContentLoaded", function() {
     const songPlayer = document.getElementById("song");
+    const elotText = document.getElementById("elot-text");
+    const elotButton = document.getElementById("elot-button");
+
+    let currentElots = parseInt(window.localStorage.getItem("elots"), 10) || 0;
+
+    elotButton.addEventListener("click", function() {
+        currentElots += 1;
+        elotText.innerText = `Elots collected: ${currentElots}`;
+        window.localStorage.setItem("elots", currentElots);
+    });
 
     window.addEventListener("click", function() {
       if(songPlayer.paused) {
         songPlayer.volume = 0.1;
         songPlayer.play();
       }
-    })
+    });
+
+    
+
+
+
+
+
+
+
 });
 
